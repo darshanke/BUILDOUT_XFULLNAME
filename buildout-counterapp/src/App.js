ced6b276107e44b58878b72af5e368c00bdba0cc
darshan <darshanke646@gmail.com>
2024-10-05 13:20:29 +0530

    done

---
+++ buildout-counterapp/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [value,setvalue]= useState(0);
+  const handleValue=(value)=>{
+   
+    if(value==="increment"){
+      setvalue((prev)=>  prev+1)
+    }
+    else{
+      setvalue((prev)=>  prev-1<0?0:prev-1 )
+    }
+    }
+  
+  
+  return (
+    <>
+    <h1>Couter App</h1>
+    <p>Count: {value}
+    </p>
+    <button
+    onClick={()=>handleValue("increment")}
+    >Increment</button>
+    <button
+      onClick={()=>handleValue("decrement")}
+      >Decrement</button>
+    </>
+  );
+}
+
+export default App;
